4a6d8b23df5c315313d4f459325498b44b29ced5
GIANBEST <gianfrancoaccion@gmail.com>
2024-09-17 09:22:49 -0300

    Primero Commit

---
+++ src/Pages/header.tsx
@@ -0,0 +1,101 @@
+import Logo from "../assets/Logo.svg"
+import "../styles/header.css";
+import "../styles/utility.css";
+
+export default function Home() {
+    return (
+        <>
+            <header className="container py-sm">
+
+            </header>
+        </>
+    )
+}
+
+<><><><><nav className="flex items-center justify-between">
+    <img src={Logo} alt="Logo DonaFrost" width={220} height={80} />
+</nav> //Essa tag nav fecha lá em baixo com o header
+    //Essa tag nav fecha lá em baixo com o header
+    //Essa tag nav fecha lá em baixo com o header
+    //Essa tag nav fecha lá em baixo com o header
+    <div className="desktop-only">
+        <ul className="flex gap-1">
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#solution">Soluções</a>
+            </li>
+            <li>
+                <a href="#testimonials">Depoimentos</a>
+            </li>
+            <li>
+                <a href="#pricing">Preços</a>
+            </li>
+            <li>
+                <a href="#contact">Contato</a>
+            </li>
+        </ul>
+    </div></><div className="desktop-only">
+        <ul className="flex gap-1">
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#solution">Soluções</a>
+            </li>
+            <li>
+                <a href="#testimonials">Depoimentos</a>
+            </li>
+            <li>
+                <a href="#pricing">Preços</a>
+            </li>
+            <li>
+                <a href="#contact">Contato</a>
+            </li>
+        </ul>
+    </div></><div className="desktop-only">
+        <div className="flex items-center">
+            <a className="reverse-color ml-lg" href="">Login</a>
+            <Button text="Cadastre-se" />
+        </div>
+    </div></><div className="mobile-menu">
+        {showMobileMenu ?
+            <div className="mobile-menu-content">
+                <div className="container flex">
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#solution">Soluções</a>
+                        </li>
+                        <li>
+                            <a href="#testimonials">Depoimentos</a>
+                        </li>
+                        <li>
+                            <a href="#pricing">Preços</a>
+                        </li>
+                        <li>
+                            <a href="#contact">Contato</a>
+                        </li>
+                    </ul>
+                    <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper">
+                        <img src={Close} alt="ícone fechar menu" width={24} height={24} />
+                    </span>
+                </div>
+            </div>
+            :
+            <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper">
+                <img src={Menu} alt="ícone menu" width={24} height={24} />
+            </span>}
+    </div></>
+    export default function Home() {
+        //Só adicionar essa linha, esse cara é o hook useState
+        const [showMobileMenu, setShowMobileMenu] = useState(false);
+        
+            return (
+                <>
+                    <header className="container py-sm">
+                    ...resto do componente já criado
+        
